0cd1fb1ed6fd863f69c9c1543698ddab290f24c8
Stephen Mohler <7486steve@gmail.com>
2023-08-29 16:35:55 -0700

    Create function that makes computer choice

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-<script>
-    
+
+//Create a variable that contains the value of a random computer choice: Rock, Paper, or Scissors
 
 
+let computerChoice = pickcomputerChoice();
+console.log(computerChoice);
 
+function pickcomputerChoice() {
+  const randomNum = Math.random()
 
+  let computerChoice = ''
 
+  if(randomNum > 0 && randomNum < 1/3) {
+    computerChoice = 'rock'
+  } else if(randomNum > 1/3 && randomNum < 2/3) {
+    computerChoice = 'paper'
+  } else if(randomNum > 2/3 && randomNum < 1) {
+    computerChoice = 'scissors'
+  }
 
+  return(computerChoice);
+}
 
-
-
-
-</script>
+]
