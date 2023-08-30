d903cc50538375b45202e1601bb92036f76a8cc9
Stephen Mohler <7486steve@gmail.com>
2023-08-30 09:59:41 -0700

    Create function playGame

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 
-//Create a variable that contains the value of a random computer choice: Rock, Paper, or Scissors
+//Create a variable that contains the value of a random COMPUTER choice: Rock, Paper, or Scissors
+//Create a variable that contains the value of a random PLAYER choice: Rock, Paper, or Scissors.
 
-
-let computerChoice = pickcomputerChoice();
+let computerChoice = pickComputerChoice();
 console.log(computerChoice);
 
-function pickcomputerChoice() {
+let playerChoice = pickPlayerChoice();
+console.log(playerChoice);
+
+function pickComputerChoice() {
   const randomNum = Math.random()
 
   let computerChoice = ''
@@ -21,4 +24,18 @@ function pickcomputerChoice() {
   return(computerChoice);
 }
 
-]
+function pickPlayerChoice() {
+  const randomNum = Math.random()
+
+  let playerChoice = ''
+
+  if(randomNum > 0 && randomNum < 1/3) {
+    playerChoice = 'rock'
+  } else if(randomNum > 1/3 && randomNum < 2/3) {
+    playerChoice = 'paper'
+  } else if(randomNum > 2/3 && randomNum < 1) {
+    playerChoice = 'scissors'
+  }
+
+  return(playerChoice);
+}
