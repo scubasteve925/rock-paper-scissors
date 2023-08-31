6f3895e24affc7a640ea971506fe9c69e8b85ef3
Stephen Mohler <7486steve@gmail.com>
2023-08-30 17:21:15 -0700

    update playRound function

--- index.js
+++ index.js
@@ -2,10 +2,11 @@
 //Create a variable that contains the value of a random COMPUTER choice: Rock, Paper, or Scissors
 //Create a comparisson that determines who wins and displays 
 
-let computerChoice = pickComputerChoice();
-let result = '';
-playGame('rock', computerChoice);
-console.log(result);
+//let userInput = prompt("Rock, Paper, or Scissors?");
+
+const playerChoice = 'rock';
+const computerChoice = pickComputerChoice();
+const result = playRound(playerChoice, computerChoice);
 
 function pickComputerChoice() {
   const randomNum = Math.random()
@@ -23,13 +24,33 @@ function pickComputerChoice() {
   return(computerChoice);
 }
 
-function playGame(playerChoice, computerChoice) {
-  if(computerChoice === playerChoice || computerChoice === playerChoice.toUpperCase() || computerChoice === playerChoice.toLowerCase()) {
-    result = 'Tie'
-  } else if(computerChoice === 'scissors') {
-    result = 'You Win. Rock beats Scissors.'
-  } else if(computerChoice === 'paper') {
-    result = 'You Lose! Paper beats Rock.'
+function playRound(playerChoice, computerChoice) {
+  
+  let result = ''
+  
+  //Player choice is Rock
+  if(playerChoice === computerChoice || playerChoice.toUpperCase() === computerChoice || playerChoice.toLowerCase() === computerChoice) {
+  result = 'Tie nobody wins.';
+  } else if(playerChoice === 'rock' && computerChoice === 'scissors') {
+  result = 'You Win! Rock beats Scissors.'
+  } else if(playerChoice === 'rock' && computerChoice === 'paper') {
+  result = 'You Lose! Paper beats Rock.'
+  }
+  //Player choice is Scissors
+  if(playerChoice === computerChoice || playerChoice.toUpperCase() === computerChoice || playerChoice.toLowerCase() === computerChoice) {
+  result = 'Tie nobody wins.';
+  } else if(playerChoice === 'scissors' && computerChoice === 'paper') {
+  result = 'You Win! Scissors beats Paper.'
+  } else if(playerChoice === 'scissors' && computerChoice === 'rock') {
+  result = 'You Lose! Rock beats Scissors.'
+  }
+  //Player choice is Paper 
+  if(playerChoice === computerChoice || playerChoice.toUpperCase() === computerChoice || playerChoice.toLowerCase() === computerChoice) {
+  result = 'Tie nobody wins.';
+  } else if(playerChoice === 'paper' && computerChoice === 'rock') {
+  result = 'You Win! Paper beats Rock.'
+  } else if(playerChoice === 'paper' && computerChoice === 'scissors') {
+  result = 'You Lose! Scissors beats Paper.'
   }
 
   return(result);
