d7687cf3f736bc43ed9cd08bc55b503ebc59048a
Stephen Mohler <7486steve@gmail.com>
2023-09-06 17:08:25 -0700

    Finish fully functioning game

--- index.js
+++ index.js
@@ -1,57 +1,90 @@
-
-//Create a variable that contains the value of a random COMPUTER choice: Rock, Paper, or Scissors
-//Create a comparisson that determines who wins and displays 
-
-//let userInput = prompt("Rock, Paper, or Scissors?");
-
-const playerChoice = 'rock';
-const computerChoice = pickComputerChoice();
-const result = playRound(playerChoice, computerChoice);
-
-function pickComputerChoice() {
-  const randomNum = Math.random()
-
-  let computerChoice = ''
-
-  if(randomNum > 0 && randomNum < 1/3) {
-    computerChoice = 'rock'
-  } else if(randomNum > 1/3 && randomNum < 2/3) {
-    computerChoice = 'paper'
-  } else if(randomNum > 2/3 && randomNum < 1) {
-    computerChoice = 'scissors'
+function getComputerChoice() {
+  const randomNum = Math.random();
+  let result = ''
+  if(randomNum < 1/3) {
+  result = 'rock'
+  } else if(randomNum > 2/3) {
+  result = 'paper'
+  } else {
+  result = 'scissors'
   }
-
-  return(computerChoice);
+return result
 }
 
-function playRound(playerChoice, computerChoice) {
-  
-  let result = ''
-  
-  //Player choice is Rock
-  if(playerChoice === computerChoice || playerChoice.toUpperCase() === computerChoice || playerChoice.toLowerCase() === computerChoice) {
-  result = 'Tie nobody wins.';
-  } else if(playerChoice === 'rock' && computerChoice === 'scissors') {
-  result = 'You Win! Rock beats Scissors.'
-  } else if(playerChoice === 'rock' && computerChoice === 'paper') {
-  result = 'You Lose! Paper beats Rock.'
-  }
-  //Player choice is Scissors
-  if(playerChoice === computerChoice || playerChoice.toUpperCase() === computerChoice || playerChoice.toLowerCase() === computerChoice) {
-  result = 'Tie nobody wins.';
-  } else if(playerChoice === 'scissors' && computerChoice === 'paper') {
-  result = 'You Win! Scissors beats Paper.'
-  } else if(playerChoice === 'scissors' && computerChoice === 'rock') {
-  result = 'You Lose! Rock beats Scissors.'
-  }
-  //Player choice is Paper 
-  if(playerChoice === computerChoice || playerChoice.toUpperCase() === computerChoice || playerChoice.toLowerCase() === computerChoice) {
-  result = 'Tie nobody wins.';
-  } else if(playerChoice === 'paper' && computerChoice === 'rock') {
-  result = 'You Win! Paper beats Rock.'
-  } else if(playerChoice === 'paper' && computerChoice === 'scissors') {
-  result = 'You Lose! Scissors beats Paper.'
-  }
+let score = {
+  wins: 0,
+  losses: 0,
+  ties: 0,
+}
 
-  return(result);
+function playRound(playerSelection, computerSelection) {
+
+let result = ''
+computerSelection = getComputerChoice();
+if(playerSelection === 'rock') {
+  if(playerSelection === computerSelection) {
+  result = 'Tie'
+  } else if (computerSelection === 'paper') {
+  result = 'You Lose'
+  } else if(computerSelection === 'scissors') {
+  result = 'You Win'  
+  }
+}
+
+if(playerSelection === 'paper') {
+  if(playerSelection === computerSelection) {
+  result = 'Tie'
+  } else if (computerSelection === 'scissors') {
+  result = 'You Lose'
+  } else if(computerSelection === 'rock') {
+  result = 'You Win'  
+  }
+}
+
+if(playerSelection === 'scissors') {
+  if(playerSelection === computerSelection) {
+  result = 'Tie'
+  } else if (computerSelection === 'rock') {
+  result = 'You Lose'
+  } else if(computerSelection === 'paper') {
+  result = 'You Win'  
+  }
+}
+
+let message = ''
+
+if(result === 'You Win') {
+  score.wins += 1
+  message = `${result}. ${playerSelection} beats ${computerSelection}.
+  Wins: ${score.wins} Losses: ${score.losses} Ties: ${score.ties}`
+} else if (result === 'You Lose') {
+  score.losses += 1
+  message = `${result}. ${computerSelection} beats ${playerSelection}.
+  Wins: ${score.wins} Losses: ${score.losses} Ties: ${score.ties}`
+} else {
+  score.ties += 1
+  message = `${result}
+  Wins: ${score.wins} Losses: ${score.losses} Ties: ${score.ties}`
+}
+
+return message
+}
+
+let computerSelection = getComputerChoice();
+let playerSelection = prompt("Chose rock, paper, or scissors", undefined).toUpperCase().toLowerCase();
+
+game();
+
+function game() {
+
+console.log(playRound(playerSelection, computerSelection));
+playerSelection = prompt("Chose rock, paper, or scissors", undefined).toUpperCase().toLowerCase();
+console.log(playRound(playerSelection, computerSelection));
+playerSelection = prompt("Chose rock, paper, or scissors", undefined).toUpperCase().toLowerCase();
+console.log(playRound(playerSelection, computerSelection));
+playerSelection = prompt("Chose rock, paper, or scissors", undefined).toUpperCase().toLowerCase();
+console.log(playRound(playerSelection, computerSelection));
+playerSelection = prompt("Chose rock, paper, or scissors", undefined).toUpperCase().toLowerCase();
+console.log(playRound(playerSelection, computerSelection));
+
 }
